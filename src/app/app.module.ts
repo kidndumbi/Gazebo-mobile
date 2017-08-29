f7b4ea841307ca78b9dad156a3393958b6710665
Cassava <kidndumbi@gmail.com>
2017-08-29 00:18:42 -0400

    add firebase config; initialize firebase modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { firebaseConfig  } from './firebaseConfig';
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+//import { FirebaseService } from '../services/firebase.service';
+
 import { MyApp } from './app.component';
 import { ListPage } from '../pages/list/list';
 
@@ -16,6 +24,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebaseConfig.ts
@@ -0,0 +1,8 @@
+ export const  firebaseConfig = {
+    apiKey: "AIzaSyDtYnXcBgdfPV-q13hJzO_ac_W6WOVVPBQ",
+    authDomain: "quizbooster.firebaseapp.com",
+    databaseURL: "https://quizbooster.firebaseio.com",
+    projectId: "quizbooster",
+    storageBucket: "quizbooster.appspot.com",
+    messagingSenderId: "1094613221385"
+  };
